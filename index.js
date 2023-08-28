eaa0db71f769fc819c92e9b774d9f8846883d80b
Felipe Soares <131181873+FelipeS0ares18@users.noreply.github.com>
2023-08-28 18:08:40 -0300

    Atualização das medias

--- index.js
+++ index.js
@@ -2,15 +2,27 @@
     const form = document.getElementById('form_atividade');
     const aprovado = '<img src="./images/icons8-verified-account-94.png" />';
     const reprovado = '<img src="./images/icons8-x-94.png" />';
+    const atividades = [];
+    const notas = [];
+    const spanAprovado = '<span class="resultado aprovado">Aprovado</span>'
+    const spanReprovado = '<span class="resultado reprovado">Reprovado</span>'
 
     let linhas = '';
 
     form.addEventListener('submit', function(e){
         e.preventDefault();
 
+        adcLinha();
+        attTabela();
+        attMedia();
+    });
+
+    function adcLinha() {
         const inputNomeAtividade = document.getElementById('nome_atividade');
         const inputNotaAtividade = document.getElementById('nota_atividade');
         
+        atividades.push(inputNomeAtividade.value);
+        notas.push(parseFloat(inputNotaAtividade.value));
 
         let linha = '<tr>';
         linha += `<td>${inputNomeAtividade.value}</td>`;
@@ -20,10 +32,29 @@
 
         linhas += linha;
 
-        const tabela = document.querySelector ('tbody');
-        tabela.innerHTML = linhas
-
         inputNomeAtividade.value = '';
         inputNotaAtividade.value = '';
-    
-    });
+    }
+
+    function attTabela () {
+        const tabela = document.querySelector ('tbody');
+        tabela.innerHTML = linhas
+    }
+
+
+    function attMedia () {
+        const mediaFinal = calculoMedia ();
+
+        document.getElementById('mediaValor').innerHTML = mediaFinal
+        document.getElementById('mediaResultado').innerHTML = mediaFinal >=  7 ? spanAprovado : spanReprovado;
+    }
+
+    function calculoMedia () {
+        let soma = 0;
+        
+        for (let i =0; i < notas.length; i++) {
+            soma += notas[i];
+        }
+
+        return soma / notas.length;
+    }
