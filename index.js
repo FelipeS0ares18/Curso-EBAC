f5d4d410e124e1dfc445e6001d9cdc7d25aff535
Felipe Soares <131181873+FelipeS0ares18@users.noreply.github.com>
2023-08-31 18:35:01 -0300

    Estrutura geral do projeto

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ $(document).ready(function() {
 
 
     $('header button').click(function(){
-        
+        $('form').slideDown();
+    })
+
+    $('#botao_cancelar').click(function() {
+        $('form').slideUp()
     })
 
     $('form').on('submit', function(e) {
         e.preventDefault();
+        const URL_img = $('#input_img').val();
+        const URL_nova_img = $('<li style="display: none"</li>');
+        $(`<img src="${URL_img}" />`).appendTo(URL_nova_img)
+
+        $(`<div class="overlay_img_link"
+            <a href="${URL_img}" target="_blank" title="Ver imagem em tamanho real"> Ver imagem em tamanho real
+            </a>
+        </div>`).appendTo(URL_nova_img)
+        $(URL_nova_img).appendTo('ul')
+        $(URL_nova_img).fadeIn();
+
+        $('#input_img').val('')
     })
 })
 
