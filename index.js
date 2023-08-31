b93b8bb11f18c2821498e7f0bc48c2f7df72614e
Felipe Soares <131181873+FelipeS0ares18@users.noreply.github.com>
2023-08-31 00:19:50 -0300

    Estrutura inicial

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+
+    $('header button').click(function(){
+        
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+    })
+})
+
